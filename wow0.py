6d6467a52f38dd7a6b2d41bd785f1796aa90a7cc
Diamantan <shokolp@gmail.com>
2016-06-13 07:09:40 -0700

    extend db before test

--- wow0.py
+++ wow0.py
@@ -1,82 +1,36 @@
 
 import pymysql
 import time
-
+import urllib.request
+import json
 
 def getFiles():
-    import urllib.request
-    import json
     requesturl = 'https://eu.api.battle.net/wow/auction/data/outland?locale=en_GB&apikey=vyaa7heguj4qysbpyxc4sbhacgera8n2'
     try:
         print('requesting url: ' + requesturl)
-        with urllib.request.urlopen(requesturl) as urlopen_return:
-            print(urlopen_return)
-            data = urlopen_return.read()
-            encoding = urlopen_return.info().get_content_charset('utf-8')
-            print(data)
-            jsonobject = json.loads(data.decode(encoding))
-            return jsonobject['files']
-
+        with urllib.request.urlopen(requesturl) as reqopen:
+            openread = reqopen.read()
+            encoding = reqopen.info().get_content_charset('utf-8')
+            print(openread)
+            reqjson = json.loads(openread.decode(encoding))
+            return reqjson
     except Exception as err:
         print(err)
 
 
-def array2db(filesarray, connection):
-    for i in filesarray:
-        if isnew(connection, i['lastModified']):
-            jsondata = url2json(i['url'])
-            json2db(jsondata, i['lastModified'], connection)
+def reqjson2db(reqjson, Connection):
+    for i in reqjson['Files']:
+        if isnew(Connection, i['lastModified']):
+            datajson = url2json(i['url'])
+            json2db(datajson, i['lastModified'], Connection)
 
-        
-def url2json(url):
-    import urllib.request
-    import json
-    jsonobject = ''
+
+
+def isnew(Connection, lastmod):
     try:
-        print('requesting url: ' + url)
-        with urllib.request.urlopen(url) as urlopen_return:
-            data = urlopen_return.read()
-            encoding = urlopen_return.info().get_content_charset('utf-8')
-            jsonobject = json.loads(data.decode(encoding))
-    except Exception as err:
-        print(err)
-    return jsonobject
-
-
-def json2db(jsondata, lastmodified, connection):
-    now = time.strftime('%Y-%m-%d %H:%M:%S')
-    try:
-        with connection.cursor() as cur:
-            querystr = 'insert into meta0 (timeinsert, timejson) values (%s, %s)'
-            cur.execute(querystr, (now, lastmodified))
-            lastrowid = cur.lastrowid
-        persistjsondata(Connection, jsondata, lastrowid)
-        connection.commit()
-        print(lastrowid)
-    except Exception as e1:
-        print (e1)
-
-
-def persistjsondata(connection, jsondata, meta0id):
-    try:
-        with connection.cursor() as cur:
-            counter = 0
-            for i in jsondata['auctions']:
-                counter = counter + 1
-                if counter % 1000 == 0:
-                    print('processed ' + str(counter) + ' lines')
-                querystr = 'insert into data0 (ownerrealm, itemid, bid, meta0id) values (%s, %s, %s, %s)'
-                cur.execute(querystr, (i['ownerRealm'], i['item'], i['bid'], meta0id))
-                
-    except Exception as err:
-        print(err)
-
-
-def isnew(connection, lastmod):
-    try:
-        with connection.cursor() as curr:
+        with Connection.cursor() as curs:
             querystr = 'select * from meta0 where timejson=%s'
-            query_return = curr.execute(querystr, lastmod)
+            query_return = curs.execute(querystr, lastmod)
             if query_return > 0:
                 return False
             else:
@@ -85,13 +39,61 @@ def isnew(connection, lastmod):
         print(err)
     return False
 
+        
+def url2json(dataurl):
+    datajson = ''
+    try:
+        print('requesting url: ' + dataurl)
+        with urllib.request.urlopen(dataurl) as dataopen:
+            openread = dataopen.read()
+            print(openread)
+            encoding = dataopen.info().get_content_charset('utf-8')
+            datajson = json.loads(openread.decode(encoding))
+    except Exception as err:
+        print(err)
+    return datajson
+
+
+def json2db(datajson, lastmodified, connection):
+    now = time.strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        with connection.cursor() as curs:
+            querystr = 'insert into meta (timeinsert, timejson) values (%s, %s)'
+            curs.execute(querystr, (now, lastmodified))
+            lastrowid = curs.lastrowid
+        json2dbdata(Connection, datajson, lastrowid)
+        connection.commit()
+        print(lastrowid)
+    except Exception as e1:
+        print (e1)
+
+
+def json2dbdata(connection, datajson, metaid):
+    try:
+        with connection.cursor() as cur:
+            counter = 0
+            for i in datajson['auctions']:
+                counter = counter + 1
+                if counter % 1000 == 0:
+                    print(str(counter) + ' lines processed')
+                querystr = 'insert into data (ownerrealm, itemid, bid, metaid) values (%s, %s, %s, %s)'
+                cur.execute(querystr, (i['ownerRealm'], i['item'], i['bid'], metaid))
+    except Exception as err:
+        print(err)
+
+
 while True:
     try:
-        filesarray = getFiles()
-        print('\n\n' + str(filesarray) + '\n')
-        Connection = pymysql.connect(host='localhost', user='root', password='patsanpizdos', 
-                     db='wowauction',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor )
-        array2db(filesarray, Connection)
+        reqjsonfiles = getFiles()
+        print('\n' + str(reqjsonfiles) + '\n')
+        Connection = pymysql.connect(
+            host='localhost', 
+            user='root', 
+            password='patsanpizdos', 
+            db='wowauction',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor)
+        reqjson2db(reqjsonfiles, Connection)
         Connection.close()
     except Exception as e1:
         print (e1)
--- wowdb.sql
+++ wowdb.sql
@@ -4,7 +4,7 @@ create database wowauction;
 
 use wowauction;
 
-create table meta0
+create table meta
 (
 id int not null auto_increment,
 timeinsert datetime not null,
@@ -12,13 +12,22 @@ timejson varchar(100),
 primary key (id)
 );
 
-create table data0
+create table data
 (
 id int not null auto_increment,
+auc varchar(50), 
+item varchar(50), 
+owner varchar(50), 
 ownerrealm varchar(50),
-itemid varchar(30),
-bid varchar(30),
-meta0id int not null,
+itemid varchar(50),
+bid varchar(50),
+buyout varchar(50), 
+quantity varchar(50), 
+timeleft varchar(50), 
+rand varchar(50), 
+seed varchar(50), 
+context varchar(50),
+metaid int not null,
 primary key (id),
-foreign key (meta0id) references meta0(id)
+foreign key (metaid) references meta(id)
 );
