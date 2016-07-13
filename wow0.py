94ac491be721334589f96f9b1a6271e3dd83abf9
Shokodash <shokotrash@gmail.com>
2016-07-13 07:10:07 +0200

    wow0.py file updated from br0 branch

--- wow0.py
+++ wow0.py
@@ -1,100 +1,85 @@
 
 import pymysql
 import time
+import urllib.request
+import json
 
 
-def getFiles():
-    import urllib.request
-    import json
-    requesturl = 'https://eu.api.battle.net/wow/auction/data/outland?locale=en_GB&apikey=vyaa7heguj4qysbpyxc4sbhacgera8n2'
-    try:
-        print('requesting url: ' + requesturl)
-        with urllib.request.urlopen(requesturl) as urlopen_return:
-            print(urlopen_return)
-            data = urlopen_return.read()
-            encoding = urlopen_return.info().get_content_charset('utf-8')
-            print(data)
-            jsonobject = json.loads(data.decode(encoding))
-            return jsonobject['files']
-
-    except Exception as err:
-        print(err)
-
-
-def array2db(filesarray, connection):
-    for i in filesarray:
-        if isnew(connection, i['lastModified']):
-            jsondata = url2json(i['url'])
-            json2db(jsondata, i['lastModified'], connection)
-
-        
 def url2json(url):
-    import urllib.request
-    import json
-    jsonobject = ''
+    return_json = ''
     try:
         print('requesting url: ' + url)
-        with urllib.request.urlopen(url) as urlopen_return:
-            data = urlopen_return.read()
-            encoding = urlopen_return.info().get_content_charset('utf-8')
-            jsonobject = json.loads(data.decode(encoding))
+        with urllib.request.urlopen(url) as openobj:
+            openread = openobj.read()
+            encoding = openobj.info().get_content_charset('utf-8')
+            return_json = json.loads(openread.decode(encoding))
     except Exception as err:
-        print(err)
-    return jsonobject
+        print('url2json: ' + str(err))
+    return return_json
 
+def reqjson2db(reqjson, Connection):
+    for i in reqjson['files']:
+        if isnew(Connection, i['lastModified']):
+            datajson = url2json(i['url'])
+            json2db(datajson, i['lastModified'], Connection)
 
-def json2db(jsondata, lastmodified, connection):
+def isnew(Connection, lastmod):
+    try:
+        with Connection.cursor() as curs:
+            querystr = 'select * from meta where timejson=%s'
+            query_return = curs.execute(querystr, lastmod)
+            if query_return > 0:
+                print('Not New')
+                return False
+            else:
+                print('New')
+                return True
+    except Exception as err:
+        print('isnew: ' + str(err))
+    return False
+
+def json2db(datajson, lastmodified, connection):
     now = time.strftime('%Y-%m-%d %H:%M:%S')
     try:
-        with connection.cursor() as cur:
-            querystr = 'insert into meta0 (timeinsert, timejson) values (%s, %s)'
-            cur.execute(querystr, (now, lastmodified))
-            lastrowid = cur.lastrowid
-        persistjsondata(Connection, jsondata, lastrowid)
+        with connection.cursor() as curs:
+            querystr = 'insert into meta (timeinsert, timejson) values (%s, %s)'
+            curs.execute(querystr, (now, lastmodified))
+            lastrowid = curs.lastrowid
+        json2dbdata(Connection, datajson, lastrowid)
         connection.commit()
-        print(lastrowid)
-    except Exception as e1:
-        print (e1)
+        print('lastrowid: ' + str(lastrowid))
+    except Exception as err:
+        print ('json2db: ' + str(err))
 
-
-def persistjsondata(connection, jsondata, meta0id):
+def json2dbdata(connection, datajson, metaid):
     try:
         with connection.cursor() as cur:
             counter = 0
-            for i in jsondata['auctions']:
+            for i in datajson['auctions']:
                 counter = counter + 1
                 if counter % 1000 == 0:
-                    print('processed ' + str(counter) + ' lines')
-                querystr = 'insert into data0 (ownerrealm, itemid, bid, meta0id) values (%s, %s, %s, %s)'
-                cur.execute(querystr, (i['ownerRealm'], i['item'], i['bid'], meta0id))
-                
+                    print(str(counter) + ' lines processed')
+                querystr = 'insert into data (auc, item, owner, ownerrealm, bid, buyout, quantity, timeleft, rand, seed, context, metaid) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+                cur.execute(querystr, (i['auc'], i['item'], i['owner'], i['ownerRealm'], i['bid'], i['buyout'], i['quantity'], i['timeLeft'], i['rand'], i['seed'], i['context'], metaid))
     except Exception as err:
-        print(err)
+        print('json2dbdata: ' + str(err))
 
-
-def isnew(connection, lastmod):
-    try:
-        with connection.cursor() as curr:
-            querystr = 'select * from meta0 where timejson=%s'
-            query_return = curr.execute(querystr, lastmod)
-            if query_return > 0:
-                return False
-            else:
-                return True
-    except Exception as err:
-        print(err)
-    return False
-
+reqjsonurl = 'https://eu.api.battle.net/wow/auction/data/outland?locale=en_GB&apikey=vyaa7heguj4qysbpyxc4sbhacgera8n2'
 while True:
     try:
-        filesarray = getFiles()
-        print('\n\n' + str(filesarray) + '\n')
-        Connection = pymysql.connect(host='localhost', user='root', password='patsanpizdos', 
-                     db='wowauction',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor )
-        array2db(filesarray, Connection)
+        reqjson = url2json(reqjsonurl)
+        Connection = pymysql.connect(
+            host='localhost', 
+            user='testuser', 
+            password='ThreeFour34$', 
+            db='wowauction',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor)
+        print('Connected to MySQL server')
+        reqjson2db(reqjson, Connection)
         Connection.close()
-    except Exception as e1:
-        print (e1)
+    except Exception as err:
+        print ('Main Body: ' + str(err))
     print('Enter Sleep')
     time.sleep(300)
     print('Exit Sleep')
